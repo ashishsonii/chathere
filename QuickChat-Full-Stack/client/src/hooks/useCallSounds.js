c569fbd0b60fc6dc8b41b20ecc71371c453c9619
Ashish Soni <ashish12217@gmail.com>
2026-06-07 04:55:19 +0530

    Add alarm sound for incoming calls

--- QuickChat-Full-Stack/client/src/hooks/useCallSounds.js
+++ QuickChat-Full-Stack/client/src/hooks/useCallSounds.js
@@ -8,7 +8,7 @@ export const useCallSounds = () => {
         // Initialize audio objects
         // We will generate empty/simple base64 tones if these files don't exist yet, 
         // or you can add real mp3s to public/sounds/ later.
-        ringtoneRef.current = new Audio('/sounds/ringtone.mp3');
+        ringtoneRef.current = new Audio('/alarm_sound_effect.mp3');
         ringtoneRef.current.loop = true;
 
         ringbackRef.current = new Audio('/sounds/ringback.mp3');
