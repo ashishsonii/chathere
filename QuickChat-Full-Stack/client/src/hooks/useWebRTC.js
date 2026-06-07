8e6bc742ab4dbc4c8a2c09b7baa7bd91e5516ac0
Ashish Soni <ashish12217@gmail.com>
2026-06-08 04:14:11 +0530

    Bypass NAT: Inject OpenRelay public free TURN servers and max-bundle policy

--- QuickChat-Full-Stack/client/src/hooks/useWebRTC.js
+++ QuickChat-Full-Stack/client/src/hooks/useWebRTC.js
@@ -3,12 +3,31 @@ import toast from 'react-hot-toast';
 
 const ICE_SERVERS = {
     iceServers: [
+        // Standard Google STUN Servers
         { urls: 'stun:stun.l.google.com:19302' },
         { urls: 'stun:stun1.l.google.com:19302' },
         { urls: 'stun:stun2.l.google.com:19302' },
-        { urls: 'stun:stun3.l.google.com:19302' }
+        { urls: 'stun:stun3.l.google.com:19302' },
+        // Public Free TURN Servers (Metered OpenRelay)
+        {
+            urls: 'turn:openrelay.metered.ca:80',
+            username: 'openrelayproject',
+            credential: 'openrelayproject'
+        },
+        {
+            urls: 'turn:openrelay.metered.ca:443',
+            username: 'openrelayproject',
+            credential: 'openrelayproject'
+        },
+        {
+            urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+            username: 'openrelayproject',
+            credential: 'openrelayproject'
+        }
     ],
-    iceCandidatePoolSize: 10
+    iceCandidatePoolSize: 10,
+    bundlePolicy: 'max-bundle',
+    rtcpMuxPolicy: 'require'
 };
 
 export const useWebRTC = (socket, userId, axios) => {
